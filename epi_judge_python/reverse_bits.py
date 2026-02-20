a6d26a2bc9f31279c3fadbee15c78f591e02f1a9
garcia-eddie <eddiegarcia.cs@gmail.com>
2026-02-20 00:56:19 -0800

    Add 4.3 solution

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,15 @@
 from test_framework import generic_test
 
 
-def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+def reverse_bits(n: int) -> int:
+    n = ((n & 0xFFFFFFFF00000000) >> 32) | ((n & 0x00000000FFFFFFFF) << 32)
+    n = ((n & 0xFFFF0000FFFF0000) >> 16) | ((n & 0x0000FFFF0000FFFF) << 16)
+    n = ((n & 0xFF00FF00FF00FF00) >> 8) | ((n & 0x00FF00FF00FF00FF) << 8)
+    n = ((n & 0xF0F0F0F0F0F0F0F0) >> 4) | ((n & 0x0F0F0F0F0F0F0F0F) << 4)
+    n = ((n & 0xCCCCCCCCCCCCCCCC) >> 2) | ((n & 0x3333333333333333) << 2)
+    n = ((n & 0xAAAAAAAAAAAAAAAA) >> 1) | ((n & 0x5555555555555555) << 1)
+
+    return n
 
 
 if __name__ == '__main__':
